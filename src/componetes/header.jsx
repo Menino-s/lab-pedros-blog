8209f3ecbfd37a1ef17dbbd2b056a999807d028a
pdrinme <pmenino28@gmail.com>
2026-02-16 03:04:35 -0300

    hover in the logo of website

--- src/componetes/header.jsx
+++ src/componetes/header.jsx
@@ -12,7 +12,7 @@ export default function Headr() {
     <>
           <div class="flex items-center justify-center p-2.5">
             <header class="bg-green-500 rounded-2xl p-5 shadow-xl/50">
-              <Link to="/"> <img src={Logo} class="items-center-safe"></img></Link>
+              <Link to="/"> <img src={Logo} class="items-center-safe transition hover:scale-115"></img></Link>
               <p class="text-center text-lg text-shadow-2xs text-shadow-black text-white font-snpro">Welcome to my old internet website</p>
             </header>
           </div>
