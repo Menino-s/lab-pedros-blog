42ca2a865208b31569f0b1a8cb8c2534ef584814
pdrinme <pmenino28@gmail.com>
2026-04-24 17:13:14 -0300

    Hot fix

--- src/posts/links-posts/Firtspost.jsx
+++ src/posts/links-posts/Firtspost.jsx
@@ -17,7 +17,7 @@ function FirtsPost() {
                     <h1 class="text-4xl font-snpro">First long post</h1>
                     <span class="text-[13px] align-text-top font-snpro">16:01 pm 04/24/2026</span>
                     <div class="grid justify-center">
-                        <img src="../img-posts/post-img-9.png" class="max-w-4xl rounded-2xl"/>
+                        <img src="../img-posts/post-img-9.png" class="lg:max-w-4xl xs:max-w-80 rounded-2xl"/>
                     </div>
                     <p class="p-4">This is the first post to have a dedicated page; not all posts will have this, most will remain just a short post. But even so, I'm very happy about this new phase :)</p>           
                 </div>
