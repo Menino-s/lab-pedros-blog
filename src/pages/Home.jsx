5b6a31f0dfabcc76b85c0f4d3a1bb5ab2a272b46
pdrinme <pmenino28@gmail.com>
2026-04-30 18:53:25 -0300

    hot fix mobilie

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,16 +19,16 @@ function Home() {
         <Stickers />
         <Headr />
         <div class="grid justify-center">
-          <div class="grid bg-amber-500 p-3 rounded-t-2xl gap-1 max-w-400 h-screen">
+          <div class="grid bg-amber-500 p-3 rounded-t-2xl gap-1 max-w-400 min-h-screen">
             <div class="grid gap-3">
               <div class="bg-white dark:bg-[#181a1b] inset-shadow-sm inset-shadow-white/40 rounded-t-2xl p-2">
                 <h1 class="text-white text-2xl text-center">This is my old internet website!</h1>
                 <p class="text-gray-200 text-md p-2 pt-0">Hi, this is my old internet blog, a little corner where I always try to make some posts or just stay. I'm not from the blogging era, but one day I stopped and thought, "Why can't I have my own blog?" Is it too old-fashioned for me? You bet it is, but I don't mind. I love that old internet aesthetic, so I've adopted it here :)</p>
               </div>
             </div>
-            <div class="grid grid-cols-3 gap-2 h-65">
+            <div class="grid lg:grid-cols-3 xs:grid-cols-1 gap-2 h-65">
               <div class="bg-white dark:bg-[#181a1b] inset-shadow-sm inset-shadow-white/40 rounded-t-2xl p-2 h-65">
-                <div class="pb-25">
+                <div class="lg:pb-25 xs:pb-0">
                   <h2 class="text-white text-xl text-center">Some tags</h2>
                   <p class="text-white text-md pt-2">Here are some tags that might represent me, since much of what I post on this blog is related to something from here!</p>
                 </div>
@@ -43,14 +43,14 @@ function Home() {
               </div>
               <div class="grid bg-white dark:bg-[#181a1b] inset-shadow-sm inset-shadow-white/40 rounded-t-2xl p-2 gap-2">
                 <h3 class="text-center text-white text-xl">Songs I've been listening to this week</h3>
-                <div class="relative h-50 w-full">
-                  <img src="https://mfswordsworth.com/wp-content/uploads/2024/10/IMG_2811-1.jpg" class="absolute left-1/2 top-0 -translate-x-1/2 z-20 max-w-[220px]" />
-                  <img src={cd} class="absolute left-55 top-4 z-10 max-w-50 animate-spin" />
+                <div class="relative lg:h-50 xs:h-30 w-full">
+                  <img src="https://mfswordsworth.com/wp-content/uploads/2024/10/IMG_2811-1.jpg" class="absolute left-1/2 top-0 -translate-x-1/2 z-20 lg:max-w-[220px] xs:max-w-[120px]" />
+                  <img src={cd} class="absolute lg:left-55 xs:left-43 top-4 z-10 lg:max-w-50 xs:max-w-25 animate-spin" />
                 </div>
                 <h4 class="text-white text-lg text-center">Chromakopia</h4>
                 <p class="text-white text-md text-center">Tyler, the Creator</p>
               </div>
-              <div>
+              <div class="lg:block xs:hidden">
                 <iframe src="https://www3.cbox.ws/box/?boxid=3554800&boxtag=VN9wkA" width="100%" height="350" allowtransparency="yes" allow="autoplay" frameborder="0" marginheight="0" marginwidth="0" scrolling="auto"></iframe>	
               </div>
             </div>
