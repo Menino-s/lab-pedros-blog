d05e177d2c8cd4b655a6617b6328456229681771
pdrinme <pmenino28@gmail.com>
2026-05-11 02:08:50 -0300

    Background update

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -5,7 +5,7 @@ import Footer from './Footer';
 const LayoutWrapper = ({ children }) => {
   return (
       <div class="relative bg-[url(/background.jpeg)] bg-cover bg-no-repeat bg-fixed cursor-[url('./assets/mouse1.png')],_pointer min-h-screen">
-      <div class="absolute inset-0 bg-black/50 bg-[radial-gradient(circle,rgba(255,255,255,0.5)_1px,transparent_1px)] bg-size-[10px_10px] pointer-events-none"></div>
+      <div class="absolute inset-0 bg-black/50 bg-[radial-gradient(circle,rgba(255,255,255,0.1)_1px,transparent_1px)] bg-size-[4px_4px] pointer-events-none"></div>
       <div class="relative z-10">
         <Headr />
         <Stickers />
