53e91af15475f6d3f3cde31a018cda6c10d067b2
pdrinme <pmenino28@gmail.com>
2026-05-26 15:05:04 -0300

    Homepage repage, new status update, new blog post and corretion in posts on blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -20,7 +20,9 @@ function Blog() {
                           <ReactPlayer src={posts.video} width="100%" height="100%" />
                         </div>
                       )}
-                      {posts.img && <img src={posts.img} class="justify-center rounded-2xl"/>}
+                      <div class="grid justify-center overflow-hidden">
+                        {posts.img && <img src={posts.img} class="rounded-2xl"/>}
+                      </div>
                         <h1 class="text-2xl font-snpro">{posts.title}</h1>
                         <span class="text-[9px] align-text-top font-snpro">{posts.hour}</span>
                         <p>{posts.content}</p>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,16 +23,24 @@ function Home() {
                       <div class="p-2 xs:grid xs:justify-center">
                         <iframe class="bg-white border-4 rounded-xl border-green-200/30" width="314" height="321" scrolling="no" src="https://gifypet.neocities.org/pet/pet.html?name=Cake&dob=1777655291&gender=f&element=Earth&pet=https%3A%2F%2Fimg.pokemondb.net%2Fsprites%2Fblack-white%2Fanim%2Fnormal%2Fpikachu-f.gif&map=https%3A%2F%2Fimages.stockcake.com%2Fpublic%2F9%2F7%2F9%2F97908976-dccf-4bdf-8f66-81f787b0c28e_medium%2Fenchanted-forest-path-stockcake.jpg&background=&tablecolor=%23529d32&textcolor=black" frameborder="0"></iframe>
                       </div>
-                      <div class="grid content-baseline gap-2">
-                        <Container>
-                          <h2 class="text-xl">This is the cake!</h2>
-                          <p class="dark:text-gray-200 text-black text-md p-2 pt-">Cake is the mascot of my website! Remember to treat her very well; she always tells me everything that happens here when I'm not around. Be nice to her, pet her, and don't forget to feed her when you come here :)</p>
-                        </Container>
-                        <Container>
-                            <h2 class="text-xl">Status Update</h2>
-                            <p class="text-xs text-gray-300/60">Posted 05/18/2026</p>
-                            <p>Very Happy &gt;ᴗ&lt;</p>
-                        </Container>
+                      <div class="grid content-baseline">
+                        <div class="grid gap-11">
+                          <div>
+                            <Container>
+                              <h2 class="text-xl">This is the cake!</h2>
+                              <p class="dark:text-gray-200 text-black text-md p-2 pt-">Cake is the mascot of my website! Remember to treat her very well; she always tells me everything that happens here when I'm not around. Be nice to her, pet her, and don't forget to feed her when you come here :)</p>
+                            </Container>
+                          </div>
+                          <div>
+                            <Container>
+                              <div class="h-30">
+                                <h2 class="text-2xl">Status Update</h2>
+                                <p class="text-xs text-gray-300/60">Posted 05/26/2026</p>
+                                <p class="text-md p-1">kind of meh ¬_¬</p>
+                              </div>
+                            </Container>
+                          </div>
+                        </div>
                       </div>
                     </div>
                     <div class="grid lg:grid-cols-2 p-2 gap-3 momento_div">
@@ -69,7 +77,7 @@ function Home() {
                       </Container>
                     </div>
               </section>
-              <section class="grid lg:grid-cols-2 gap-1 content-baseline right_div">
+              <section class="grid lg:grid-cols-2 gap-3 content-baseline right_div">
                     <Container class="tags_div">
                       <div class="lg:pb-10 xs:pb-0">
                         <h2 class=" text-xl text-center">Some tags</h2>
@@ -120,22 +128,31 @@ function Home() {
                               <div class="rounded-xl bg-gray-300/80 backdrop-blur-2xl inset-shadow-sm inset-shadow-white/40 flex justify-center p-2">
                                 <img src={meinmii} class="w-45" />
                               </div>
-                                <div class="group flex justify-center transition">
+                                <div class="group flex justify-center transition overflow-hidden">
                                     <img src={miinormal} class="w-80 block group-hover:hidden" />
-                                    <img src={miihello} class="w-80 hidden group-hover:block" />
+                                    <img src={miihello} class="w-80 hidden group-hover:block group-hover:scale-105" />
                                 </div>
                             </div>
                     </div>
                     </div>
-                    <Container class="cutepic_div">
-                      <p class="text-center text-md text-shadow-2xs/60 text-shadow-black">Contact? <br/><span class="text-center text-xs text-shadow-2xs/60 text-shadow-black">(I dunno, maybe won't reply)</span></p>
-                      <div class="grid gap-3 p-4 text-center">
-                        <a href="mailto:pedro.menino.carlos@gmail.com" class="flex justify-items-center gap-2.5 p-1.5 bg-green-800 rounded-xl shadow-xl/15 font-comicsans inset-shadow-sm inset-shadow-white/80 transition hover:scale-115 hover:shadow-xl/50 hover:bg-green-800/60 text-white"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Email_Shiny_Icon.svg/330px-Email_Shiny_Icon.svg.png" class="h-10" /> Email</a>
-                        <a href="https://www.instagram.com/pdrinme/" class="flex justify-items-center gap-2.5 p-1.5 bg-green-800 rounded-xl shadow-xl/15 font-comicsans inset-shadow-sm inset-shadow-white/80 transition hover:scale-115 hover:shadow-xl/50 hover:bg-green-800/60 text-white"><img src="https://i.redd.it/n50b6goja95d1.png" class="h-10 rounded-xl" /> Instagram</a>
-                        <a href="https://x.com/pdrinme" class="flex justify-items-center gap-2.5 p-1.5 bg-green-800 rounded-xl shadow-xl/15 font-comicsans inset-shadow-sm inset-shadow-white/80 transition hover:scale-115 hover:shadow-xl/50 hover:bg-green-800/60 text-white"><img src="https://i.redd.it/a-collection-of-some-of-my-skeuomorphic-icons-v0-z11v3xc5guad1.png?width=1675&format=png&auto=webp&s=c7d796e88c6caf356734a3eb227962282e8203bd" class="h-10 rounded-xl" /> X (Formerly called Twitter)</a>
-                        <a href="/404" class="relative group flex justify-items-center gap-2.5 p-1.5 bg-green-800 rounded-xl shadow-xl/15 font-comicsans inset-shadow-sm inset-shadow-white/80 transition hover:scale-115 hover:shadow-xl/50 hover:bg-red-800/60 text-white"><img src="https://i.redd.it/7tjlv2zxisaf1.png" class="h-10 rounded-xl grayscale-0 group-hover:grayscale-100" /> Discord <span class="rotate-12 absolute top-3 right-0.5 border-2 border-red-900/40 bg-yellow-500 hidden group-hover:block transition group-hover:border-4 group-hover:bg-red-900 rounded-xl p-0.5">Out of stock</span></a>
+                    <div class="grid lg:gap-9 xs:gap-0.5">
+                      <div>
+                        <Container class="cutepic_div">
+                          <p class="text-center text-md text-shadow-2xs/60 text-shadow-black">Contact? <br/><span class="text-center text-xs text-shadow-2xs/60 text-shadow-black">(I dunno, maybe won't reply)</span></p>
+                          <div class="grid gap-3 p-4 text-center">
+                            <a href="mailto:pedro.menino.carlos@gmail.com" class="flex justify-items-center gap-2.5 p-1.5 bg-green-800 rounded-xl shadow-xl/15 font-comicsans inset-shadow-sm inset-shadow-white/80 transition hover:scale-115 hover:shadow-xl/50 hover:bg-green-800/60 text-white"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Email_Shiny_Icon.svg/330px-Email_Shiny_Icon.svg.png" class="h-10" /> Email</a>
+                            <a href="https://www.instagram.com/pdrinme/" class="flex justify-items-center gap-2.5 p-1.5 bg-green-800 rounded-xl shadow-xl/15 font-comicsans inset-shadow-sm inset-shadow-white/80 transition hover:scale-115 hover:shadow-xl/50 hover:bg-green-800/60 text-white"><img src="https://i.redd.it/n50b6goja95d1.png" class="h-10 rounded-xl" /> Instagram</a>
+                            <a href="https://x.com/pdrinme" class="flex justify-items-center gap-2.5 p-1.5 bg-green-800 rounded-xl shadow-xl/15 font-comicsans inset-shadow-sm inset-shadow-white/80 transition hover:scale-115 hover:shadow-xl/50 hover:bg-green-800/60 text-white"><img src="https://i.redd.it/a-collection-of-some-of-my-skeuomorphic-icons-v0-z11v3xc5guad1.png?width=1675&format=png&auto=webp&s=c7d796e88c6caf356734a3eb227962282e8203bd" class="h-10 rounded-xl" /> X (Formerly called Twitter)</a>
+                            <a href="/404" class="relative group flex justify-items-center gap-2.5 p-1.5 bg-green-800 rounded-xl shadow-xl/15 font-comicsans inset-shadow-sm inset-shadow-white/80 transition hover:scale-115 hover:shadow-xl/50 hover:bg-red-800/60 text-white"><img src="https://i.redd.it/7tjlv2zxisaf1.png" class="h-10 rounded-xl grayscale-0 group-hover:grayscale-100" /> Discord <span class="rotate-12 absolute top-3 right-0.5 border-2 border-red-900/40 hidden group-hover:block transition group-hover:bg-red-900 rounded-xl p-0.5">Out of stock</span></a>
+                          </div>
+                        </Container>
                       </div>
-                    </Container>
+                      <div>
+                        <Container>
+                          <img src="https://cdn.dribbble.com/userupload/22148904/file/original-56b19720d83b146e748d69554e264aff.jpg?resize=752x&vertical=center" class="rounded-xl" />
+                        </Container>
+                      </div>
+                    </div>
               </section>
             </div>
           </Divmain>
